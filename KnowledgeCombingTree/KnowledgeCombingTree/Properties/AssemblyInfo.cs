b4d291025777f4947341040e321977a72a8ba4f5
linwh8 <675176683@qq.com>
2017-05-06 15:13:42 +0800

    fundamental UI

--- KnowledgeCombingTree/KnowledgeCombingTree/Properties/AssemblyInfo.cs
+++ KnowledgeCombingTree/KnowledgeCombingTree/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KnowledgeCombingTree")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ KnowledgeCombingTree/KnowledgeCombingTree/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using KnowledgeCombingTree.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace KnowledgeCombingTree.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
