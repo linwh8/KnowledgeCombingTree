0c3e8de43e3f25824bf2fed7a45dd3c5e440a3ca
linwh8 <675176683@qq.com>
2017-05-07 14:01:20 +0800

    Delete Privacy Statement

--- KnowledgeCombingTree/KnowledgeCombingTree/ViewModels/MainPageViewModel.cs
+++ KnowledgeCombingTree/KnowledgeCombingTree/ViewModels/MainPageViewModel.cs
@@ -51,8 +51,8 @@ namespace KnowledgeCombingTree.ViewModels
         public void GotoSettings() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 0);
 
-        public void GotoPrivacy() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 1);
+        //public void GotoPrivacy() =>
+          //  NavigationService.Navigate(typeof(Views.SettingsPage), 1);
 
         public void GotoAbout() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 2);
